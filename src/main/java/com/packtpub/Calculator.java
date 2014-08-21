2f37b87f1e880aa4436d789ca59a0dce1600c414
maryanne-exe <maryannexe@ya.ru>
2014-08-21 19:11:17 +0400

    commit_00

--- src/main/java/com/packtpub/Calculator.java
+++ src/main/java/com/packtpub/Calculator.java
@@ -3,7 +3,7 @@ package com.packtpub;
 public class Calculator
 {
     public int sum(int x, int y)
-    {
-        return x + y;
-    }
+{
+    return x + y;
+}
 }
--- src/test/com/packtpub/CalculatorTest.java
+++ src/test/com/packtpub/CalculatorTest.java
@@ -8,6 +8,12 @@ public class CalculatorTest extends TestCase {
     public void testSum() throws Exception {
         Calculator calculator = new Calculator();
         int sum = calculator.sum(1, 2);
+
         Assert.assertEquals(3, sum);
     }
+
+    public void testBad() throws Exception {
+        Assert.fail("Some bad test has failed");
+    }
+
 }
