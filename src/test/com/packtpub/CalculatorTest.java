eebf1d50f39b197337546da801337b70ad4bcc09
maryanne-exe <maryannexe@ya.ru>
2014-08-21 19:24:40 +0400

    commit_01

--- src/test/com/packtpub/CalculatorTest.java
+++ src/test/com/packtpub/CalculatorTest.java
@@ -5,19 +5,7 @@ package com.packtpub;
 
 public class CalculatorTest extends TestCase {
 
-    public void testSum() throws Exception {
-        Calculator calculator = new Calculator();
-        int sum = calculator.sum(1, 2);
-
-        Assert.assertEquals(3, sum);
-    }
-
     public void testBad() throws Exception {
         Assert.fail("Some bad test has failed");
     }
-
-    public void testVeryBad() throws Exception {
-        Assert.fail("Some very bad test has failed");
-    }
-
 }
